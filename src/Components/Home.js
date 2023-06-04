2625674cf023834af6d5564e6c1e558a51f7a513
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-06-04 16:28:48 +0530

    commit

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div>
+      <img class="home_car" src='https://i.pinimg.com/474x/bb/6c/52/bb6c5247b5b71ae58383594a93f76007.jpg' alt="home" />
+    </div>
+  )
+}
+
+export default Home
